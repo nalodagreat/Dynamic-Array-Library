755f366be16d1bb3d855fcdb8687f8f2c5fa480a
nalodagreat <nalothegreat0@gmail.com>
2025-08-27 00:01:42 +0200

    set array value and size

---
+++ Dynamic-Array-Library/MyDynamicArray.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <class T>
+class MyDynamicArray
+{
+protected:
+    int _size;
+public:
+    T* arr;
+
+    MyDynamicArray(int size = 0)
+    {
+        if (size <= 0)
+            _size = 0;
+        else
+            _size = size;
+
+        arr = (_size > 0) ? new T[_size] : nullptr;
+    }
+
+    ~MyDynamicArray()
+    {
+        delete[] arr;
+    }
+
+    bool setArray(int index, T value)
+    {
+        if (index < 0 || index >= _size)
+            return false;
+
+        arr[index] = value;
+        return true;
+    }
+
+    
+};
